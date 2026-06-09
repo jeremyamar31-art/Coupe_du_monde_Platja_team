b2d75826ae71521670af9721457f3a8904e6476b
Louangecepaix <jeremy.amar31@gmail.com>
2026-06-09 09:42:25 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "XXX",
-  authDomain: "XXX",
-  projectId: "XXX",
-  storageBucket: "XXX",
-  messagingSenderId: "XXX",
-  appId: "XXX"
+  apiKey: "AIzaSyBc5UmXi7td8riY52n3ON35Utsa4W22GJY",
+  authDomain: "coupe-du-monde-8f658.firebaseapp.com",
+  projectId: "coupe-du-monde-8f658",
+  storageBucket: "coupe-du-monde-8f658.firebasestorage.app",
+  messagingSenderId: "249687884895",
+  appId: "1:249687884895:web:31880a612b49c2c27007ea"
 };
 
 firebase.initializeApp(firebaseConfig);
